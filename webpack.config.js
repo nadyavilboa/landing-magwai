01f9166d3ae95557ced1456a764e09775b760348
nadya <nadya.vilboa@list.ru>
2023-09-23 20:41:29 +0300

    mobile style

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -26,12 +27,25 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader,
                 
                 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png|svg|jpg|jpeg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]'
+                }
             }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'style.css'
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets/img', to: 'img' },
+                { from: 'src/index.html' },
+            ]
+        }),
     ],
 }
