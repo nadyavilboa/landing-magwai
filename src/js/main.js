8d439eb177516fd310dc4893f398b45ede978a4e
nadya <nadya.vilboa@list.ru>
2023-09-24 22:38:29 +0300

    desktop style, menu, modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+const menu = document.querySelector('.navigation');
+const menuBtn = document.querySelector('#menu-toggle');
+
+const modalBtn = document.querySelectorAll('.btn-modal');
+const modalCloseBtn = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+
+console.log('test');
+
+const menuToggle = () => {
+    menu.classList.toggle('navigation__visible');
+}
+
+const modalOpen = () => {
+    modal.classList.remove('d-none');
+    modal.classList.add('modal-visible');
+}
+
+const modalClose = () => {
+    modal.classList.add('d-none');
+    modal.classList.remove('modal-visible');
+}
+
+menuBtn.addEventListener('click', () => menuToggle());
+
+modalBtn.forEach((btn) => btn.addEventListener('click', () => modalOpen()));
+modalCloseBtn.addEventListener('click', () => modalClose());
