0278ad83bbd94f53c3ae953077254ef8521173bb
nadya <nadya.vilboa@list.ru>
2023-09-25 12:49:08 +0300

    final style, check posts

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,7 @@ const menuBtn = document.querySelector('#menu-toggle');
 const modalBtn = document.querySelectorAll('.btn-modal');
 const modalCloseBtn = document.querySelector('.modal__close');
 const modal = document.querySelector('.modal');
-
-console.log('test');
+const modalWrapper = modal.querySelector('.modal__wrapper');
 
 const menuToggle = () => {
     menu.classList.toggle('navigation__visible');
@@ -14,14 +13,27 @@ const menuToggle = () => {
 const modalOpen = () => {
     modal.classList.remove('d-none');
     modal.classList.add('modal-visible');
+    modalWrapper.classList.remove('animation-fadeOut');
+    modalWrapper.classList.add('animation-fadeIn');
 }
 
 const modalClose = () => {
-    modal.classList.add('d-none');
-    modal.classList.remove('modal-visible');
+    modalWrapper.classList.remove('animation-fadeIn');
+    modalWrapper.classList.add('animation-fadeOut');
+    setTimeout(() => {
+        modal.classList.add('d-none');
+        modal.classList.remove('modal-visible');
+    }, 500);
+
 }
 
 menuBtn.addEventListener('click', () => menuToggle());
 
 modalBtn.forEach((btn) => btn.addEventListener('click', () => modalOpen()));
 modalCloseBtn.addEventListener('click', () => modalClose());
+
+document.addEventListener('keydown', function (e) {
+    if(e.keyCode === 27) {
+        modalClose();
+    }
+});
